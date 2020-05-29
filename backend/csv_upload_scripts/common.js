0dd9e2666c588fcac2adde197b9e65dc39c07199
Casey Roby <elenacaseyroby@gmail.com>
2020-05-29 16:38:39 -0400

    add set helper functions

--- backend/csv_upload_scripts/common.js
+++ backend/csv_upload_scripts/common.js
@@ -9,3 +9,24 @@ export function cleanString(string) {
   }
   return cleanString;
 }
+
+export function unionOfTwoArrays(a, b) {
+  // contains the elements of both set a and set b.
+  const setA = new Set(a);
+  const setB = new Set(b);
+  return Array(new Set([...setA, ...setB]));
+}
+
+export function intersectionOfTwoArrays(a, b) {
+  // contains those elements of set a that are also in set b.
+  const setA = new Set(a);
+  const setB = new Set(b);
+  return Array(new Set([...setA].filter((x) => setB.has(x))));
+}
+
+export function differenceOfTwoArrays(a, b) {
+  // contains those elements of set a that are not in set b.
+  const setA = new Set(a);
+  const setB = new Set(b);
+  return Array(new Set([...setA].filter((x) => !setB.has(x))));
+}
