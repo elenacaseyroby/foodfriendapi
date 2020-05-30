2b87a5e4558456aaf8d410009f3a1f3c3302f279
Casey Roby <elenacaseyroby@gmail.com>
2020-05-30 14:06:01 -0400

    add db level unique contraint on appropriate join tables

--- backend/migrations/000013-create-nutrient-benefit.js
+++ backend/migrations/000013-create-nutrient-benefit.js
@@ -1,30 +1,36 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('nutrient_benefits', {
-      nutrient_id: {
-        allowNull: false,
-        type: Sequelize.INTEGER,
-        references: { model: 'nutrients', key: 'id' },
-      },
-      benefit_id: {
-        allowNull: false,
-        type: Sequelize.INTEGER,
-        references: { model: 'benefits', key: 'id' },
-      },
-      created_at: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-      },
-      updated_at: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal(
-          'CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'
-        ),
-      },
-    });
+    return queryInterface
+      .createTable('nutrient_benefits', {
+        nutrient_id: {
+          allowNull: false,
+          type: Sequelize.INTEGER,
+          references: { model: 'nutrients', key: 'id' },
+        },
+        benefit_id: {
+          allowNull: false,
+          type: Sequelize.INTEGER,
+          references: { model: 'benefits', key: 'id' },
+        },
+        created_at: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        },
+        updated_at: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal(
+            'CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'
+          ),
+        },
+      })
+      .then(() => {
+        return queryInterface.sequelize.query(
+          'ALTER TABLE `nutrient_benefits` ADD UNIQUE `unique_index`(`nutrient_id`, `benefit_id`);'
+        );
+      });
   },
   down: (queryInterface, Sequelize) => {
     return queryInterface.dropTable('nutrient_benefits');
