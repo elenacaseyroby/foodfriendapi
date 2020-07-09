1f78b684d057f8a5463fc5d782f4fa9861ac4bc5
Casey Roby <elenacaseyroby@gmail.com>
2020-07-09 16:13:37 -0400

    move signup tests into own module

--- backend/__tests__/auth.js
+++ backend/__tests__/auth.js
@@ -1,9 +1,4 @@
-import {
-  checkUserIsLoggedIn,
-  signUp,
-  sendPasswordResetEmail,
-  resetPassword,
-} from '../services/auth';
+import { sendPasswordResetEmail, resetPassword } from '../services/auth';
 import chai from 'chai';
 import { db } from '../models';
 
@@ -16,13 +11,6 @@ describe('auth', async () => {
   before(async function () {
     // runs before all tests in this file regardless where this line is defined.
 
-    // Create terms & privacy policy.
-    const terms = await db.TermsAndConditions.create({
-      text: 'some junk',
-    });
-    const policy = await db.PrivacyPolicy.create({
-      text: 'some privacy junk',
-    });
     // Delete any users already in test db.
     const allUsers = await db.User.findAll({});
     allUsers.forEach((user) => {
@@ -30,36 +18,13 @@ describe('auth', async () => {
     });
     // Delete emails
     const allEmails = await db.Email.findAll({});
-    allEmails.forEach((email) => {
-      email.destroy();
+    allEmails.forEach(async (email) => {
+      await email.destroy();
     });
-    // Delete agreements
-    const allPolicyAgreements = await db.UserPrivacyPolicies.findAll({});
-    allPolicyAgreements.forEach(async (p) => {
-      await p.destroy();
-    });
-    const allTermsAgreements = await db.UserTermsAndConditions.findAll({});
-    allTermsAgreements.forEach(async (t) => {
-      await t.destroy();
-    });
-    return terms && policy;
   });
   after(async function () {
     // runs before all tests in this file regardless where this line is defined.
 
-    // Delete terms & privacy policy.
-    const terms = await db.TermsAndConditions.findOne({
-      where: {
-        text: 'some junk',
-      },
-    });
-    const policy = await db.PrivacyPolicy.findOne({
-      where: {
-        text: 'some privacy junk',
-      },
-    });
-    terms.destroy();
-    policy.destroy();
     // Delete users.
     const allUsers = await db.User.findAll({});
     allUsers.forEach((user) => {
@@ -67,84 +32,9 @@ describe('auth', async () => {
     });
     // Delete emails
     const allEmails = await db.Email.findAll({});
-    allEmails.forEach((email) => {
-      email.destroy();
+    allEmails.forEach(async (email) => {
+      await email.destroy();
     });
-    // Delete agreements
-    const allPolicyAgreements = await db.UserPrivacyPolicies.findAll({});
-    allPolicyAgreements.forEach((p) => {
-      p.destroy();
-    });
-    const allTermsAgreements = await db.UserTermsAndConditions.findAll({});
-    allTermsAgreements.forEach((t) => {
-      t.destroy();
-    });
-    const allPolicies = await db.PrivacyPolicy.findAll({});
-    allPolicies.forEach((p) => {
-      p.destroy();
-    });
-    const allTerms = await db.TermsAndConditions.findAll({});
-    allTerms.forEach((t) => {
-      t.destroy();
-    });
-    return 'success';
-  });
-  it('signUp function returns response with valid accessToken', async () => {
-    const firstName = 'Dyl';
-    const lastName = 'Cam';
-    const email = 'Dylan@live.com';
-    const password = '12345678';
-    const response = await signUp(firstName, lastName, email, password);
-    expect(response.status).to.equal(200);
-    const req = {
-      headers: {
-        authorization: response.accessToken,
-      },
-    };
-    const userIsLoggedIn = checkUserIsLoggedIn(req);
-    expect(userIsLoggedIn).to.be.true;
-    // any db data created must be destroyed at end of test
-    const user = await db.User.findOne({
-      where: {
-        email: email,
-      },
-    });
-    user.destroy();
-  });
-  it('signUp function returns error when first name is empty string', async () => {
-    const firstName = '';
-    const lastName = 'Cam';
-    const email = 'Dylan@live.com';
-    const password = '12345678';
-    const response = await signUp(firstName, lastName, email, password);
-    expect(response.status).to.equal(400);
-    expect(response.message).to.equal('Please provide a name.');
-  });
-  it('signUp function agrees user to terms and privacy policy', async () => {
-    const firstName = 'Joe';
-    const lastName = 'Roby';
-    const email = 'Joe@robes.com';
-    const password = '12345678';
-    const response = await signUp(firstName, lastName, email, password);
-    expect(response.status).to.equal(200);
-    // Check that terms are agreed.
-    const user = await db.User.findOne({
-      where: {
-        email: email,
-      },
-    });
-    const latestTerms = await db.TermsAndConditions.findOne({
-      order: [['datePublished', 'DESC']],
-    });
-    const userAgreedToTerms = await user.hasAgreedToTerms(latestTerms);
-    const latestPolicy = await db.PrivacyPolicy.findOne({
-      order: [['datePublished', 'DESC']],
-    });
-    const userAgreedToPrivacy = await user.hasAgreedToPrivacyPolicy(
-      latestPolicy
-    );
-    expect(userAgreedToTerms && userAgreedToPrivacy).to.be.true;
-    user.destroy();
   });
   it('send password reset email', async () => {
     const momentsAgo = Date();
---
+++ backend/__tests__/auth/signUp.js
@@ -0,0 +1,123 @@
+import { checkUserIsLoggedIn, signUp } from '../../services/auth';
+import chai from 'chai';
+import { db } from '../../models';
+
+require('dotenv').config();
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
+const { expect } = chai;
+
+describe('auth', async () => {
+  before(async function () {
+    // runs before all tests in this file regardless where this line is defined.
+
+    // Create terms & privacy policy.
+    const terms = await db.TermsAndConditions.create({
+      text: 'some junk',
+    });
+    const policy = await db.PrivacyPolicy.create({
+      text: 'some privacy junk',
+    });
+    // Delete any users already in test db.
+    const allUsers = await db.User.findAll({});
+    allUsers.forEach(async (user) => {
+      await user.destroy();
+    });
+    // Delete user agreements
+    const allPolicyAgreements = await db.UserPrivacyPolicies.findAll({});
+    allPolicyAgreements.forEach(async (p) => {
+      await p.destroy();
+    });
+    const allTermsAgreements = await db.UserTermsAndConditions.findAll({});
+    allTermsAgreements.forEach(async (t) => {
+      await t.destroy();
+    });
+    return terms && policy;
+  });
+  after(async function () {
+    // runs before all tests in this file regardless where this line is defined.
+
+    // Delete users.
+    const allUsers = await db.User.findAll({});
+    allUsers.forEach(async (user) => {
+      await user.destroy();
+    });
+    // Delete terms & privacy policy.
+    const allPolicies = await db.PrivacyPolicy.findAll({});
+    allPolicies.forEach(async (p) => {
+      await p.destroy();
+    });
+    const allTerms = await db.TermsAndConditions.findAll({});
+    allTerms.forEach(async (t) => {
+      await t.destroy();
+    });
+    // Delete user agreements
+    const allPolicyAgreements = await db.UserPrivacyPolicies.findAll({});
+    allPolicyAgreements.forEach((p) => {
+      p.destroy();
+    });
+    const allTermsAgreements = await db.UserTermsAndConditions.findAll({});
+    allTermsAgreements.forEach((t) => {
+      t.destroy();
+    });
+    return 'success';
+  });
+  it('signUp function returns response with valid accessToken', async () => {
+    const firstName = 'Dyl';
+    const lastName = 'Cam';
+    const email = 'Dylan@live.com';
+    const password = '12345678';
+    const response = await signUp(firstName, lastName, email, password);
+    expect(response.status).to.equal(200);
+    const req = {
+      headers: {
+        authorization: response.accessToken,
+      },
+    };
+    const userIsLoggedIn = checkUserIsLoggedIn(req);
+    expect(userIsLoggedIn).to.be.true;
+    // any db data created must be destroyed at end of test
+    const user = await db.User.findOne({
+      where: {
+        email: email,
+      },
+    });
+    user.destroy();
+  });
+  it('signUp function returns error when first name is empty string', async () => {
+    const firstName = '';
+    const lastName = 'Cam';
+    const email = 'Dylan@live.com';
+    const password = '12345678';
+    const response = await signUp(firstName, lastName, email, password);
+    expect(response.status).to.equal(400);
+    expect(response.message).to.equal('Please provide a name.');
+  });
+  it('signUp function agrees user to terms and privacy policy', async () => {
+    const firstName = 'Joe';
+    const lastName = 'Roby';
+    const email = 'Joe@robes.com';
+    const password = '12345678';
+    const response = await signUp(firstName, lastName, email, password);
+    expect(response.status).to.equal(200);
+    // Check that terms are agreed.
+    const user = await db.User.findOne({
+      where: {
+        email: email,
+      },
+    });
+    const latestTerms = await db.TermsAndConditions.findOne({
+      order: [['datePublished', 'DESC']],
+    });
+    const userAgreedToTerms = await user.hasAgreedToTerms(latestTerms);
+    const latestPolicy = await db.PrivacyPolicy.findOne({
+      order: [['datePublished', 'DESC']],
+    });
+    const userAgreedToPrivacy = await user.hasAgreedToPrivacyPolicy(
+      latestPolicy
+    );
+    expect(userAgreedToTerms && userAgreedToPrivacy).to.be.true;
+    // any db data created must be destroyed at end of test
+    user.destroy();
+  });
+});
