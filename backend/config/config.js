5efd597bb8d5d7aba0c74643bfa992bb24a1e8ad
Casey Roby <elenacaseyroby@gmail.com>
2020-10-05 15:00:23 -0400

    connects to aws db

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,27 +2,12 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    operatorsAliases: true,
+    DB_URL: process.env.DB_URL_DEV,
   },
   test: {
-    username: process.env.TEST_DB_USERNAME,
-    password: process.env.TEST_DB_PASSWORD,
-    database: process.env.TEST_DB_NAME,
-    host: process.env.TEST_DB_HOST,
-    dialect: 'mysql',
-    operatorsAliases: true,
+    DB_URL: process.env.DB_URL_TEST,
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    operatorsAliases: true,
+    DB_URL: process.env.DB_URL_PROD,
   },
 };
