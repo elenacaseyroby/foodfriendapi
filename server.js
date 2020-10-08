155ea83f6117baad70ace833627f340993b11346
Casey Roby <elenacaseyroby@gmail.com>
2020-10-08 17:38:37 -0400

    connect backend to frontend

--- server.js
+++ server.js
@@ -43,12 +43,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'frontend/public')));
+app.use(express.static(path.join(__dirname, 'frontend/build')));
 
 // The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
+// match one below, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/frontend/public/index.html'));
+  res.sendFile(path.join(__dirname + '/frontend/build/index.html'));
 });
 
 // Config bodyparser for handling api request data.
