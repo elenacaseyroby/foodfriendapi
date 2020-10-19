1c90ce43eff16e49ee199c50caf69dd554fa363a
Casey Roby <elenacaseyroby@gmail.com>
2020-10-19 13:53:18 -0400

    make sure tests always run on test db db

--- config/config.js
+++ config/config.js
@@ -9,10 +9,10 @@ module.exports = {
     dialect: 'mysql',
   },	
   test: {	
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: process.env.DB_USERNAME_TEST,
+    password: process.env.DB_PASSWORD_TEST,
+    database: process.env.DB_NAME_TEST,
+    host: process.env.DB_HOST_TEST,
     dialect: 'mysql',
   },	
   production: {	
