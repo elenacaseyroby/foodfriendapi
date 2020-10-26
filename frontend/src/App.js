7113af04a53fd643666d0edd819fdb9041cd0f58
Casey Roby <elenacaseyroby@gmail.com>
2020-10-26 14:59:16 -0400

    delete unnecessary files & move fonts into assets folder & add comingsoon page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import AboutUs from './components/AboutUs';
+import AboutUs from './components/ComingSoon';
 import './App.css';
 
 class App extends Component {
