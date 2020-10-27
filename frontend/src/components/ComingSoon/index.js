7ed02b8464b7e147096288ea46ca5bb994e11f16
Casey Roby <elenacaseyroby@gmail.com>
2020-10-26 23:38:07 -0400

    bottom elipse not sticking to bottom

--- frontend/src/components/ComingSoon/index.js
+++ frontend/src/components/ComingSoon/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import header from './assets/header.svg';
 import benny from './assets/benny-and-food.svg';
+import foodfriendLogo from './assets/foodfriend-type-logo.svg';
+import bottomElipse from './assets/bottom-elipse.svg';
 
 class AboutUs extends Component {
   render() {
@@ -8,11 +10,23 @@ class AboutUs extends Component {
     const styles = getStyles(media);
     return (
       <div style={styles.comingSoonPage}>
-        <div style={styles.header} />
-        {/* <div style={styles.elipseContainer}>
+        <div style={styles.headerAndBennyContainer}>
+          <div style={styles.header} />
+          <img src={benny} style={styles.benny}/>
+        </div>
+        <div style={styles.logoContainer}>
+          <div style={styles.line} />
+          <img src={foodfriendLogo} style={styles.foodfriendLogo} />
+        </div>
+        <div style={styles.footerContainer}>
+          {/* <img src={bottomElipse} style={styles.elipse} /> */}
           <div style={styles.elipse} />
-        </div> */}
-        <img src={benny} style={styles.benny}/>
+        </div>
+
+          {/* <div style={styles.elipseContainer}>
+          <div style={styles.elipse} />
+          </div> */}
+
       </div>
     );
   }
@@ -30,6 +44,12 @@ const getStyles = (media) => {
       backgroundColor: '#ffffff',
       postion: 'relative',
     },
+    headerAndBennyContainer: {
+      display: 'flex',
+      width: '100%',
+      postion: 'relative',
+      flexDirection: 'column',
+    },
     header: {
       width: '100%',
       height: windowHeight / 2,
@@ -46,6 +66,48 @@ const getStyles = (media) => {
       // aspectRatio: width / height,
       aspectRatio: 604 / 418,
       alignSelf: 'center',
+    },
+    logoContainer: {
+      display: 'flex',
+      width: '100%',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    // line: {
+    //   borderBottomStyle: 'solid',
+    //   borderWidth: .5,
+    //   width: '100%',
+    //   color: '#555555',
+    //   postion: 'absolute',
+    //   // alignSelf: 'center',
+    // },
+    foodfriendLogo: {
+      postion: 'absolute',
+      backgroundColor: '#ffffff',
+      padding: 40 * normalizer,
+      width: 200,
+      height: undefined,
+      // aspectRatio: width / height,
+      aspectRatio: 201 / 37,
+    },
+    footerContainer: {
+      display: 'flex',
+      width: '100%',
+      flexDirection: 'column',
+      // borderStyle: 'dotted',
+      postion: 'absolute',
+      bottom: 0,
+    },
+    elipse: {
+      width: '100%',
+      height: windowHeight / 4,
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover',
+      backgroundImage: `url(${bottomElipse})`,
+      postion: 'absolute',
+      bottom: 0,
     },
     // elipseContainer: {
     //   width: '90%',
@@ -69,13 +131,14 @@ const getStyles = (media) => {
   };
   if (device === 'mobile') {
     styles.benny.width = 300;
-    styles.benny.marginTop = - windowHeight/4;
+    styles.benny.marginTop = - windowHeight/4.5;
   } else if (device === 'tablet') {
     styles.benny.width = 400;
     styles.benny.marginTop = - windowHeight/3;
   } else {
     styles.benny.width = 550;
     styles.benny.marginTop = - windowHeight/2.2;
+    styles.foodfriendLogo.width = 300 * normalizer;
   }
   return styles;
 }
