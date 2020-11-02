cbbdf7581f3d30251bad336ac48cdbe959a507ee
Casey Roby <elenacaseyroby@gmail.com>
2020-11-02 14:35:52 -0500

    works

--- frontend/src/components/ComingSoon/index.js
+++ frontend/src/components/ComingSoon/index.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import header from './assets/header.svg';
 import benny from './assets/benny-and-food.svg';
 import foodfriendLogo from './assets/foodfriend-type-logo.svg';
-import bottomElipse from './assets/bottom-elipse.svg';
 import banner from './assets/banner.svg';
 
 class AboutUs extends Component {
@@ -11,17 +10,21 @@ class AboutUs extends Component {
     const styles = getStyles(media);
     return (
       <div style={styles.comingSoonPage}>
-        <div style={styles.headerAndBennyContainer}>
-          <div style={styles.header} />
-          <img src={benny} style={styles.benny} alt={'benny-with-food'}/>
+        <div style={styles.headerContainer}>
+          <img src={header} style={styles.headerImg} alt={'header'}/>
+          <div style={styles.headerColorBlock}/>
+          <img src={benny} style={styles.benny} alt={'benny'}/>
         </div>
         <div style={styles.logoContainer}>
+          <img src={foodfriendLogo} style={styles.logo} alt={'logo'} />
           <div style={styles.line} />
-          <img src={foodfriendLogo} style={styles.foodfriendLogo} alt={'foodfriend-logo'}/>
         </div>
-        <div style={styles.elipseContainer}>
-          <img src={bottomElipse} style={styles.elipse} alt={'bottom-elipse'}/>
-          <div style={styles.orangeFiller} />
+        <div style={styles.bannerContainer}>
+          <img src={banner} style={styles.banner} alt={'banner'} />
+          <div style={styles.bannerTextContainer}>
+            <div style={styles.h1} >Coming Soon</div>
+            <div style={styles.h2} >contactmyfoodfriend@gmail.com</div>
+          </div>
         </div>
       </div>
     );
@@ -29,98 +32,131 @@ class AboutUs extends Component {
 }
 
 const getStyles = (media) => {
-  const {windowHeight, normalizer, device} = media;
+  const {windowHeight, windowWidth, normalizer, device} = media;
   let styles = {
     comingSoonPage: {
-      display: 'flex',
-      width: '100%',
-      minHeight: '100vh',
-      height: '100%',
-      flexDirection: 'column',
-      backgroundColor: '#ffffff',
-      postion: 'relative',
+      width: windowWidth,
+      minHeight: windowWidth / 3,
+      height: windowHeight,
     },
-    headerAndBennyContainer: {
+    headerContainer: {
       display: 'flex',
-      width: '100%',
-      postion: 'relative',
       flexDirection: 'column',
+      width: windowWidth,  
+      overflow: 'hidden',
+      position: 'relative',
+      // weird fix: for some reason this div has larger height than the sum of its children.
+      maxHeight: windowHeight / 1.7,
     },
-    header: {
-      width: '100%',
-      height: windowHeight / 2,
-      backgroundPosition: 'center',
-      backgroundRepeat: 'no-repeat',
-      backgroundSize: 'cover',
-      backgroundImage: `url(${header})`,
-      postion: 'absolute',
+    headerImg: {
+      width: 1440,
+      position: 'relative',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      zIndex: 0,
     },
     benny: {
-      postion: 'absolute',
-      height: undefined,
-      aspectRatio: 604 / 418,
+      position: 'absolute',
       alignSelf: 'center',
+      marginTop: windowHeight / 10,
+      zIndex: 2,
     },
     logoContainer: {
       display: 'flex',
-      width: '100%',
+      marginTop: windowHeight / 90,
+      marginBottom: windowHeight / 8,
+      position: 'relative',
       flexDirection: 'column',
       justifyContent: 'center',
-      alignItems: 'center',
+      zIndex: 1,
     },
-    // line: {
-    //   borderBottomStyle: 'solid',
-    //   borderWidth: .5,
-    //   width: '100%',
-    //   color: '#555555',
-    //   postion: 'absolute',
-    //   // alignSelf: 'center',
-    // },
-    foodfriendLogo: {
-      postion: 'absolute',
+    line: {
+      borderBottom: '0.01em solid #555555',
+    },
+    logo: {
+      alignSelf: 'center', 
+      padding: '2%',
+      position: 'absolute',
+      zIndex: 1,
       backgroundColor: '#ffffff',
-      padding: 40 * normalizer,
-      width: 200,
-      height: undefined,
-      // aspectRatio: width / height,
-      aspectRatio: 201 / 37,
     },
-    elipseContainer: {
-      marginTop: 45 * normalizer,
+    bannerContainer: {
       display: 'flex',
+      marginTop: '2em',
       width: '100%',
       flexDirection: 'column',
-      postion: 'absolute',
-      bottom: 0,
-      flex: '1 1 auto',
     },
-    elipse: {
-      width: '100%',
-      postion: 'relative',
-      bottom: 0,
-      height: undefined,
-      aspectRatio: 1440 / 193,
-    },
-    orangeFiller: {
-      marginTop: -2,
-      height: '100%',
-      width: '100%',
-      flex: '1 1 auto',
-      backgroundColor: '#ffe3b8',
+    banner: {
+      alignSelf: 'center',
       position: 'relative',
+      zIndex: 0,
     },
+    bannerTextContainer: {
+      display: 'flex',
+      position: 'absolute',
+      zIndex: 1,
+      marginTop: '.25em',
+      justifyContent: 'center',
+      alignItems: 'center',
+      alignSelf: 'center',
+      flexDirection: 'column',
+    },
+    h1: {
+      fontFamily: 'Cabin-Regular',
+      color: '#555555',
+    },
+    h2: {
+      fontFamily: 'Cabin-Regular',
+      color: '#555555',
+    },
+
   };
-  if (device === 'mobile') {
-    styles.benny.width = 300;
-    styles.benny.marginTop = - windowHeight/4.5;
+  // hide color block header by default
+  styles.headerColorBlock = {
+    height: 0,
+    width: 0,
+  };
+  let bennyWidth = 400;
+  if  (device === 'mobile') {
+    bennyWidth = windowWidth - 10;
+    styles.benny = {
+      alignSelf: 'center',
+      zIndex: 1,
+      marginTop: - normalizer * 600,
+    };
+    // hide header image on mobile
+    styles.headerImg = {
+      height: 0,
+      width: 0,
+    };
+    styles.headerColorBlock = {
+      width: '100%',
+      height: 1200 * normalizer,
+      backgroundColor: '#ffd99f',
+      zIndex: 0,
+    };
+    styles.logoContainer.marginTop = windowHeight / 20;
+    styles.logoContainer.marginBottom = windowHeight / 10;
   } else if (device === 'tablet') {
-    styles.benny.width = 400;
-    styles.benny.marginTop = - windowHeight/3;
-  } else {
-    styles.benny.width = 550;
-    styles.benny.marginTop = - windowHeight/2.2;
-    styles.foodfriendLogo.width = 300 * normalizer;
+    styles.logoContainer.marginTop = windowHeight / 90;
+    styles.logoContainer.marginBottom = windowHeight / 10;
+  } else if (device === 'desktop') {
+    styles.logoContainer.marginTop = windowHeight / 90;
+    styles.logoContainer.marginBottom = windowHeight / 9;
+  } else if (windowWidth > 1440) {
+    bennyWidth = 400 * normalizer;
   }
+  styles.benny.width = bennyWidth;
+  styles.logo.width = bennyWidth / 3;
+  styles.banner.width = bennyWidth;
+  styles.bannerTextContainer.width = bennyWidth;
+  styles.h1.fontSize = bennyWidth / 20;
+  styles.h2.fontSize = bennyWidth / 30;
+  // make sure "FoodFriend" is not cut off when window height is reduced.
+  if (styles.logoContainer.marginTop < 20) {
+    styles.logoContainer.marginTop = 20;
+  }
+
   return styles;
 }
 
