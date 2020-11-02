54d7e33f2df94f4c62e5b490b3ee66b386d94dbd
Casey Roby <elenacaseyroby@gmail.com>
2020-11-02 15:47:13 -0500

    different config dbs for different environments

--- config/config.js
+++ config/config.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 
 module.exports = {	
   development: {	
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: process.env.DB_USERNAME_DEV,
+    password: process.env.DB_PASSWORD_DEV,
+    database: process.env.DB_NAME_DEV,
+    host: process.env.DB_HOST_DEV,
     dialect: 'mysql',
   },	
   test: {	
@@ -16,10 +16,10 @@ module.exports = {
     dialect: 'mysql',
   },	
   production: {	
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: process.env.DB_USERNAME_PROD,
+    password: process.env.DB_PASSWORD_PROD,
+    database: process.env.DB_NAME_PROD,
+    host: process.env.DB_HOST_PROD,
     dialect: 'mysql',
   },
 }
